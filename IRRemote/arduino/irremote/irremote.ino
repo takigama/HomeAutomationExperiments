4fbe8f063e6930f6bad2f6ba641ca61e8cad97ab
Paul J R <me@pjr.cc>
2017-04-09 01:38:18 +1000

    its kinda working... some ir codes work, some dont

--- IRRemote/arduino/irremote/irremote.ino
+++ IRRemote/arduino/irremote/irremote.ino
@@ -12,7 +12,6 @@
 #define FW_NAME "esp8266-IR-send-recv-temp-light";
 #define FW_VERSION "0.0.1a"
 
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Homie.h>
@@ -23,7 +22,8 @@
 // pins that stuff hangs off
 int IR_RECV_PIN = 4; //an IR detector/demodulator is connected to GPIO pin 2
 int TEMP_PIN = 12;
-int IR_SEND_PING = 5;
+int IR_SEND_PIN = 5;
+bool ignoreloop = false;
 
 OneWire onewire(TEMP_PIN);
 DallasTemperature dTemp(&onewire);
@@ -34,7 +34,7 @@ DallasTemperature dTemp(&onewire);
 // this var holds the last sent temperature
 float cTemp = 0.0;
 float fTemp = 0.0;
-unsigned long cMillis=0;
+unsigned long cMillis=0,mMillis=0;
 unsigned long lastTempMillis=0;
 
 // ir results
@@ -42,7 +42,7 @@ decode_results results;
 
 
 // temp check interval
-unsigned long tc_interval = 60000;
+unsigned long tc_interval = 60000ul;
 
 
 // define out homie components
@@ -52,7 +52,7 @@ HomieNode AmbLight("ambientlight", "ambientlight");
 
 // setup our ir send and recv
 IRrecv irrecv(IR_RECV_PIN);
-IRsend irsend(IR_SEND_PING);
+IRsend irsend(IR_SEND_PIN);
 
 
 
@@ -60,13 +60,16 @@ IRsend irsend(IR_SEND_PING);
 
 void doTemp()
 {
-  if(cMillis < lastTempMillis || ((cMillis-lastTempMillis) > tc_interval)) {
+  if((cMillis-lastTempMillis) > tc_interval) {
     // update temp.... um.. i forgot how.
 
     // get a temp in both C and F
     dTemp.requestTemperatures(); 
     cTemp = dTemp.getTempCByIndex(0);
     fTemp = dTemp.getTempFByIndex(0);
+    Serial.println("would send temps");
+    Serial.println(String(cTemp));
+    Serial.println(String(fTemp));
 
     // dont bother sending a temp if it doesnt make sense
     if(cTemp > -100.0 && cTemp < 100) {
@@ -84,7 +87,7 @@ void doTemp()
 
 void doIrRecv(decode_results *r)
 {
-  IR.setProperty("irreceived").send("read");
+  IR.setProperty("irreceived").send("recd");
   String toSend;
 
   switch(r->decode_type) {
@@ -103,6 +106,18 @@ void doIrRecv(decode_results *r)
     case PANASONIC:
       toSend = "PANASONIC,"+String(r->bits) + "," +String(r->value,HEX);
     break;
+    case SAMSUNG:
+      toSend = "SAMSUNG,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case SANYO:
+      toSend = "SANYO,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case MITSUBISHI:
+      toSend = "MITSUBISHI,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case DISH:
+      toSend = "DISH,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
     case LG:
       toSend = "LG,"+String(r->bits) + "," +String(r->value,HEX);
     break;
@@ -115,12 +130,48 @@ void doIrRecv(decode_results *r)
     case WHYNTER:
       toSend = "WHYNTER,"+String(r->bits) + "," +String(r->value,HEX);
     break;
+    case SHARP:
+      toSend = "SHARP,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case COOLIX:
+      toSend = "COOLIX,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case DAIKIN:
+      toSend = "DAIKIN,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case DENON:
+      toSend = "DENON,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case KELVINATOR:
+      toSend = "KELVINATOR,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case SHERWOOD:
+      toSend = "SHERWOOD,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
+    case MITSUBISHI_AC:
+      toSend = "MITSUBISHI_AC ,"+String(r->bits) + "," +String(r->value,HEX);
+    break;
     case UNKNOWN:
       toSend = "UNKNOWN,"+String(r->bits) + "," +String(r->value,HEX);
     break;
   }
 
+  Serial.println(toSend);
   IR.setProperty("irreceived").send(toSend);
+  Serial.print("unsigned int  ");          // variable type
+  Serial.print("rawData[");                // array name
+  Serial.print(r->rawlen - 1, DEC);  // array size
+  Serial.print("] = {");                   // Start declaration
+
+  // Dump data
+  for (int i = 1;  i < r->rawlen;  i++) {
+    Serial.print(r->rawbuf[i] * USECPERTICK, DEC);
+    if ( i < r->rawlen-1 ) Serial.print(","); // ',' not needed on last one
+    if (!(i & 1))  Serial.print(" ");
+  }
+
+  // End declaration
+  Serial.println("};");  // 
 }
 
 
@@ -151,28 +202,68 @@ String getValue(String data, char separator, int index)
 
 bool irSendHandler(HomieRange range, String value)
 {
+  ignoreloop = true;
   // decode "value" into something that makes sense.
   // should contain 3 comma sep'd values "encoding", "bits", "value";
-  IR.setProperty("irsenddebug").send("about to send");
+  //IR.setProperty("irsenddebug").send("about to send");
+  char val_c[128];
 
+  irrecv.disableIRIn();
+
+  Serial.println("about to send IR");
+  Serial.println(value);
   String enc = getValue(value, ',', 0);
   String bits = getValue(value, ',', 1);
   String val = getValue(value, ',', 2);
+  val.toCharArray(val_c, 128);
+  long rval = strtol(val_c, NULL, 16);
 
   if(enc == "NEC") {
     // for now i only handle nec cause im lazy
-    irsend.sendNEC(bits.toInt(), val.toInt());`
+//    Serial.println("now sending NEC");
+//    Serial.println(rval);
+//    Serial.println(bits.toInt());
+    irsend.sendNEC(rval,bits.toInt());
+//    Serial.println("after irsend");
+      //unsigned int  rawData[67] = {8500,4250, 600,1550, 600,500, 600,1600, 600,500, 600,500, 600,1550, 600,500, 600,1550, 600,500, 600,1550, 600,500, 600,1550, 600,1550, 600,500, 600,1550, 600,500, 600,1550, 600,1550, 600,500, 600,1550, 600,1550, 600,500, 600,500, 600,500, 600,500, 600,500, 600,1550, 600,500, 600,500, 600,1550, 600,1550, 600,1550, 600};
+      //unsigned int  rawData[67] = {8500,4250, 600,1600, 550,500, 600,1600, 550,500, 600,500, 600,1600, 550,500, 600,1600, 550,500, 550,1600, 550,500, 600,1600, 550,1600, 550,500, 600,1600, 550,500, 600,500, 600,1600, 550,500, 600,1600, 550,1600, 550,500, 600,500, 600,500, 550,1600, 550,500, 600,1600, 550,500, 600,500, 600,1600, 550,1600, 550,1600, 550};
+//      unsigned int  rawData[67] = {8500,4250, 600,1600, 550,500, 600,1600, 550,500, 600,500, 600,1600, 600,500, 600,1600, 600,500, 550,1600, 550,500, 600,1600, 550,1600, 550,500, 600,1550, 600,500, 600,500, 600,1600, 550,500, 600,1600, 550,1600, 600,500, 600,500, 600,500, 550,1600, 550,500, 600,1600, 550,500, 600,500, 600,1600, 550,1550, 600,1600, 550};
+      
+//      irsend.sendRaw(rawData, sizeof(rawData)/sizeof(rawData[0]), 56);
   }
-    
+
+  irrecv.enableIRIn();
+  ignoreloop = false;
+  return true;
 }
 
 
 
 
 
+void setupHandler()
+{
+  irrecv.enableIRIn();
+  irsend.begin();
+//  dTemp.begin();
+  
+}
 
 
 
+void loopHandler()
+{
+  if(ignoreloop) return;
+
+  
+  if (irrecv.decode(&results)) {
+    doIrRecv(&results);
+    irrecv.resume();
+  }
+
+  doTemp();
+
+}
 
 
 
@@ -180,19 +271,25 @@ void  setup()
 {
   Serial.begin(9600);
 
-  dTemp.begin();
-  
+
+  // for some reason, whatever homie does on pin 16 kills the serial - not sure why
+  // it should be pin 16 on a generic esp8266-12e board
+  Homie.setLedPin(16, false);
   Homie_setFirmware(FW_NAME, FW_VERSION);
+  Serial.println("and thus we begin");
   
-  irrecv.enableIRIn();
 
-  Homie.setup();
+  Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
 
-  //Temp("temperature", "temperature");
   //HomieNode IR("ir", "ir");
   //HomieNode AmbLight("ambientlight", "ambientlight");
-  Temp.advertise("ir").settable(irSendHandler);
-
+  Temp.advertise("temperaturef");
+  Temp.advertise("temperaturec");
+  IR.advertise("irsend").settable(irSendHandler);
+  IR.advertise("irreceived");
+  
+  
+  Homie.setup();
 }
 
 
@@ -202,14 +299,10 @@ void  setup()
 
 void loop()
 {
+  if(ignoreloop) return;
   cMillis = millis();
 
-  if (irrecv.decode(&results)) {
-    doIrRecv(&results);
-    irrecv.resume();
-  }
-  
-  doTemp();
+
   
   Homie.loop();
 }
